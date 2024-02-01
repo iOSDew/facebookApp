a348d99484cbe2c4ee761b26ff9d72d865c441d0
Hojimurod <hojimurodsultonov2004@gmail.com>
2024-02-01 23:19:01 +0500

    Initial Commit

---
+++ facebookApp/facebookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookAppApp.swift
+//  facebookApp
+//
+//  Created by Khojimurod Sultonov on 01.02.24.
+//
+
+import SwiftUI
+
+@main
+struct facebookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
